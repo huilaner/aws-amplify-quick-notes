73c59186e3324103a473a3a9df027c8b43ac64f5
huilaner <huibiology@gmail.com>
2021-01-02 08:49:33 -0800

    update title

--- src/components/Screens.js
+++ src/components/Screens.js
@@ -91,7 +91,7 @@ export default () => {
   return (
     <>
       <Header>
-        <Title>Quick Notes</Title>
+        <Title>Hui\'s Quick Notes</Title>
         <SignOutButton
           onClick={() => {
             Auth.signOut().then(() => window.location.reload());
