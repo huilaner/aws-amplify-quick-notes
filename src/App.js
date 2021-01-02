49e2f94be1af22d30d759bb45dd65b2a9a386ddc
huilaner <huibiology@gmail.com>
2021-01-01 23:07:06 -0800

    test CD CI

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ function App() {
     <Screens />
   ) : (
     <>
-      <Title>Quick Notes</Title>
+      <Title>Welcome to Huis Quick Note</Title>
       <Authenticator
         onStateChange={authState => {
           if (authState === "signedIn") {
